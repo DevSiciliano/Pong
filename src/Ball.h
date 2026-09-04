24d91c17bc3d81794efca0495da3db56a827e07b
DevSiciliano <m.noto@ikmail.com>
2026-09-04 23:18:15 +0200

    feat: create Ball class

---
+++ src/Ball.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Ball
+{
+private:
+    sf::Vector2f m_Position;
+    sf::RectangleShape m_BallShape;
+
+    float m_Speed = 500;
+    float m_DirectionX = 1.f;
+    float m_DirectionY = 1.f;
+
+public:
+    Ball(float startX, float startY);
+
+    sf::FloatRect getPosition();
+    sf::RectangleShape getBallShape();
+
+    float getXVelocity();
+
+    void bounceSide();
+
+    void bounceTop();
+
+    void missBottom();
+
+    void hitBall();
+
+    void updateBall(sf::Time dt);
+
+};
