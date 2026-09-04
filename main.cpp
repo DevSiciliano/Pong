c42c3f3a9c6f9d7b2c5a361bd35890573be1bfdb
DevSiciliano <m.noto@ikmail.com>
2026-09-04 22:46:04 +0200

    feat: create pong window
    - With SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <SFML/Graphics.hpp>
 
+#include <optional>
+
 int main() {
+    sf::VideoMode VM({1280u, 720u});
+    sf::RenderWindow window(VM, "Pong Game");
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 
     return 0;
 }
